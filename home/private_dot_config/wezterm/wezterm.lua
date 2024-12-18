53797b1f640115fbe5b56a3909d675a53c40b104
Cameron Braid <cameron@braid.com.au>
2024-12-18 21:06:17 +1100

    customize

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -4,10 +4,12 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Monokai Pro Ristretto (Gogh)'
+config.color_scheme = 'Campbell (Gogh)'
+
 config.keys = {
-  { key = 'n', mods = 'CMD', action = wezterm.action.SpawnCommandInNewWindow { cwd=wezterm.home_dir } },
+  { key = 'n', mods = 'CTRL', action = wezterm.action.SpawnCommandInNewWindow { cwd=wezterm.home_dir } },
 }
+
 config.window_decorations = 'RESIZE'
 
 return config
